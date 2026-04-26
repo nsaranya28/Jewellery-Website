da126183bdf1d24ab3514373bb783427a4612fd6
nsaranya28 <nsaranya282@gmail.com>
2026-04-26 21:50:58 +0530

    error

--- test_mail_clean.php
+++ test_mail_clean.php
@@ -1,20 +1,23 @@
 <?php
 require_once 'includes/mailer.php';
 
-// Test email address
 $test_email = 'saranya02022005@gmail.com'; 
 $subject = "Test Email from Jewels.com";
 $message = "This is a test email to verify SMTP configuration.";
 
 echo "<h1>SMTP Test</h1>";
-echo "Attempting to send email to $test_email...<br>";
+echo "Attempting to send email to " . htmlspecialchars($test_email) . "...<br>";
 
-$result = send_mail_smtp($test_email, $subject, $message);
-
-if ($result === true) {
-    echo "<b style='color:green;'>SUCCESS: Email sent successfully!</b>";
+if (function_exists('send_mail_smtp')) {
+    $result = send_mail_smtp($test_email, $subject, $message);
+    if ($result === true) {
+        echo "<b style='color:green;'>SUCCESS: Email sent successfully!</b>";
+    } else {
+        echo "<b style='color:red;'>FAILED: Email sending failed.</b><br>";
+        echo "<pre>" . htmlspecialchars($result) . "</pre>";
+    }
 } else {
-    echo "<b style='color:red;'>FAILED: Email sending failed.</b><br>";
-    echo "<pre>" . htmlspecialchars($result) . "</pre>";
+    echo "<b style='color:red;'>ERROR: Function send_mail_smtp not defined!</b><br>";
+    echo "Check if includes/mailer.php is correctly included and has &lt;?php tag.";
 }
 ?>
